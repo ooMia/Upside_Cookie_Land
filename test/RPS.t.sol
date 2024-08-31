3a5ac51c53149bbcecac2cb8f8212b0f5bc4ed8b
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2024-08-31 18:32:06 +0900

    🔥 Remove : RPSMock

--- test/RPS.t.sol
+++ test/RPS.t.sol
@@ -2,9 +2,3 @@
 pragma solidity ^0.8.26;
 
 import {RPS} from "src/logic/RPS.sol";
-
-contract RPSMock is RPS {
-    function setStack(RPSData[] calldata _stack) external {
-        stack = _stack;
-    }
-}
