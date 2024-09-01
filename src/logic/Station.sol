01b3375362f09ef3bc4aa8900b6a76037354f159
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2024-09-02 00:42:44 +0900

    🚧 WIP : Station (in-progress)
    - test_step1_buyCookie
    - test_step2_approveCookie
    - test_step3_playRPS
    - test_step4_claim

--- src/logic/Station.sol
+++ src/logic/Station.sol
@@ -10,31 +10,51 @@ import {IOracle, Oracle} from "util/Oracle.sol";
 import {IGameProxy} from "util/Proxy.sol";
 
 interface IStation {
-    function buyCookie(uint256 _amount) external payable;
-
-    function sellCookie(uint256 _amount) external;
-
     function depositLP(uint256 _amount) external;
 
     function withdrawLP(uint256 _amount) external;
 
-    /// @dev Mint and deposit the coin for the amount of eth sent
-    /// @param _minimumAmount The minimum amount of coin to mint. Revert if the amount is less than this.
-    function mintDepositCookie(uint256 _minimumAmount) external payable;
-
     function playMulti(bytes[] calldata _data) external;
 }
 
+contract TransientLock {
+    modifier NonReentrant(bytes32 key) {
+        assembly {
+            if tload(key) { revert(0, 0) }
+            tstore(key, 1)
+        }
+        _;
+        assembly {
+            tstore(key, 0)
+        }
+    }
+
+    bytes32 internal constant LOCK = keccak256("lock");
+    bytes32 internal constant B_LOCK = keccak256("b");
+}
+
+contract LPVendor {
+    mapping(address => uint256) internal LP;
+
+    function getLPBalance() external view returns (uint256) {
+        return LP[msg.sender];
+    }
+
+    function depositLP(uint256 _amount) external {
+        // TODO : implement
+    }
+
+    function withdrawLP(uint256 _amount) external {
+        // TODO : implement
+    }
+}
+
 /// @dev 반드시 admin 계정을 통해서만 오라클을 설정하고 실행해야 함
+
 contract Station is CookieVendor, Ownable {
     // stoppable
     IOracle public oracle;
     address internal gameProxy;
-
-    struct User {
-        uint256 CK;
-        uint256 LP;
-    }
 
     mapping(uint256 => GameMeta) internal games;
 
@@ -47,45 +67,58 @@ contract Station is CookieVendor, Ownable {
         require(cookie.balanceOf(address(this)) == type(uint256).max);
     }
 
-    function mintDepositCookie(uint256 _minimumAmount) external payable {
-        // TODO : implement
+    /* --- External Functions --- */
+
+    function getGameMeta(uint256 _id) external view returns (GameMeta memory) {
+        return games[_id];
     }
 
-    function getLPBalance() external view returns (uint256) {
-        return users[msg.sender].LP;
-    }
-
-    function depositLP(uint256 _amount) external {
-        // TODO : implement
-    }
-
-    function withdrawLP(uint256 _amount) external {
-        // TODO : implement
-    }
-
-    function gotRandomNumbers(bytes32 _hash) external {
-        // TODO: 내부 게임에 전파하고, 개별 claim 처리
-    }
-
-    function setGame(GameMeta calldata _meta) external {
-        // only admin
+    function setGame(GameMeta calldata _meta) external onlyOwner {
         games[_meta.id] = _meta;
     }
 
+    function setGame(uint256 _id, address _logic, uint256 _version, uint256 _minAmount, uint256 _maxAmount)
+        external
+        onlyOwner
+    {
+        games[_id] = createGameMeta(_id, _logic, _version, _minAmount, _maxAmount);
+    }
+
+    function claim() external {
+        // TODO : 사용자가 플레이한 게임의 결과를 순회한다.
+        // TODO : 정보의 블록 번호의 해시를 토대로 오라클을 통해 숫자를 가져온다.
+        // TODO : 해당 게임의 결과를 계산한다.
+        // TODO : 결과에 따라 순차적으로 0.95 또는 1.95를 곱하고, 도중에 한 번이라도 지면 바로 0을 반환한다.
+        // TODO : 사용자의 reward에 결과를 누적한다.
+    }
+
     function playMulti(bytes[] calldata _data) external {
-        // TODO no reentrancy
         for (uint256 i = 0; i < _data.length; ++i) {
             (uint256 gameId, uint256 amount, bytes memory data) = abi.decode(_data[i], (uint256, uint256, bytes));
             play(gameId, amount, data);
         }
     }
 
-    function play(uint256 _gameId, uint256 _amount, bytes memory _data) public {
-        // TODO 주어진 양만큼의 코인이 있는지 확인
-        // TODO 오라클을 통해 랜덤 시드를 받아옴
-        // TODO 주어진 data에 대한 play 수행
+    function playRandom(uint256 _gameId, uint256 _amount, uint256 _length) external {
+        playRandom(_gameId, _amount, _length, 0);
+    }
 
+    /* --- Public Functions --- */
+
+    function playRandom(uint256 _gameId, uint256 _amount, uint256 _length, bytes32 _seed) public {
         GameMeta storage meta = games[_gameId];
+        authorizePlay(meta, _amount);
+        bytes memory data = abi.encodeWithSelector(IGame.playRandom.selector, _amount, _length);
+        if (_seed != 0) {
+            data = bytes.concat(data, abi.encode(_seed));
+        }
+        (bool res,) = meta.logic.delegatecall(data);
+        require(res);
+    }
+
+    function play(uint256 _gameId, uint256 _amount, bytes memory _data) public {
+        GameMeta storage meta = games[_gameId];
+        authorizePlay(meta, _amount);
         (bool res,) = meta.logic.delegatecall(abi.encodeWithSelector(IGame.play.selector, _amount, _data));
         require(res);
     }
--- test/logic/Station.t.sol
+++ test/logic/Station.t.sol
@@ -6,6 +6,8 @@ import "forge-std/Test.sol";
 import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+import {GamePlayed} from "logic/Game.sol";
+import {RPS} from "logic/RPS.sol";
 import {IStation, Station} from "logic/Station.sol";
 import {Cookie, CookieVendor} from "token/Cookie.sol";
 import {IOracle, Oracle} from "util/Oracle.sol";
@@ -21,11 +23,148 @@ contract StationTest is Test {
         station = new Station(address(game));
         cookie = station.cookie();
         oracle = station.oracle();
+        setRPS(100, 10000);
         startHoax(msg.sender);
     }
 
     function payableCallMustSucceed(uint256 _value, bytes memory _data) internal returns (bool res) {
         (res,) = address(station).call{value: _value}(_data);
         require(res);
+    }
+
+    /* --- Admin Actions --- */
+
+    function setRPS(uint256 _min, uint256 _max) public {
+        station.setGame(0, address(new RPS()), 1, _min, _max);
+    }
+
+    /* --- User Actions --- */
+
+    /* --- Buy Cookie --- */
+
+    function buyCookie_asIs(uint256 _amount) internal view {
+        assertGe(msg.sender.balance, _amount * station.getCookiePrice());
+    }
+
+    function buyCookie_toBe(uint256 _amount, uint256 _balance) internal view {
+        assertLt(msg.sender.balance, _balance);
+        assertEq(cookie.balanceOf(msg.sender), _amount);
+    }
+
+    function buyCookie(uint256 _amount) internal {
+        uint256 cost = _amount * station.getCookiePrice();
+        payableCallMustSucceed(cost, abi.encodeWithSignature("buyCookie(uint256)", _amount));
+    }
+
+    /* --- Approve Cookie --- */
+
+    function approveCookie_asIs(uint256 _amount) internal view {
+        assertGe(cookie.balanceOf(msg.sender), _amount);
+    }
+
+    function approveCookie_toBe(uint256 _amount) internal view {
+        assertEq(cookie.allowance(msg.sender, address(station)), _amount);
+    }
+
+    function approveCookie(uint256 _amount) internal {
+        cookie.approve(address(station), _amount);
+    }
+
+    /* --- Play RPS --- */
+
+    function playRPS_asIs(uint256 _amount) internal view {
+        assertGe(cookie.balanceOf(msg.sender), _amount);
+        assertGe(cookie.allowance(msg.sender, address(station)), _amount);
+    }
+
+    function playRPS_toBe(uint256 _amount, uint256 _cookieBalance) internal view {
+        assertEq(cookie.balanceOf(msg.sender), _cookieBalance - _amount);
+    }
+
+    function playRPS(uint256 _amount, uint256 _id, uint256 _len) internal {
+        vm.expectEmit(true, true, true, false, address(station));
+        emit GamePlayed(address(msg.sender), block.number, "RPS");
+        // playRandom(_gameId, _amount, _length, _seed)
+        payableCallMustSucceed(
+            _id, abi.encodeWithSignature("playRandom(uint256,uint256,uint256,bytes32)", _id, _amount, _len, 1)
+        );
+    }
+
+    /* --- Claim --- */
+
+    function claim() public {
+        (bool res,) = address(station).call(abi.encodeWithSignature("claim()"));
+        require(res);
+    }
+
+    function claim_asIs() internal view {
+        assertEq(cookie.balanceOf(address(msg.sender)), 0);
+    }
+
+    function claim_toBe() internal view {
+        assertGt(cookie.balanceOf(address(msg.sender)), 0, "No Prize");
+    }
+
+    /* --- Basic Oracle Actions --- */
+    function setRandomSeed(bytes32 _seed) public {}
+
+    /* --- Steps For Playing --- */
+
+    /// forge-config: default.fuzz.runs = 1
+    function test_step1_buyCookie(uint8 _number) public {
+        uint256 amount = _number == 0 ? vm.randomUint(1, 255) : _number;
+        uint256 _balance = msg.sender.balance;
+
+        buyCookie_asIs(amount);
+        buyCookie(amount);
+        buyCookie_toBe(amount, _balance);
+    }
+
+    /// forge-config: default.fuzz.runs = 1
+    function test_step2_approveCookie(uint8 _number) public {
+        uint256 amount = _number == 0 ? vm.randomUint(1, 255) : _number;
+        buyCookie(amount);
+
+        approveCookie_asIs(amount);
+        approveCookie(amount);
+        approveCookie_toBe(amount);
+    }
+
+    /// forge-config: default.fuzz.runs = 1
+    function test_step3_playRPS(uint8 _number) public {
+        uint256 _id = 0;
+        uint256 _min = station.getGameMeta(_id).minAmount;
+        uint256 amount = _number < _min ? vm.randomUint(_min, 255) : _number;
+
+        buyCookie(amount);
+        approveCookie(amount);
+        uint256 _balance = cookie.balanceOf(msg.sender);
+
+        playRPS_asIs(amount);
+        playRPS(amount, _id, 3);
+        playRPS_toBe(amount, _balance);
+    }
+
+    /// forge-config: default.fuzz.runs = 1
+    function test_step4_claim(uint8 _number) public {
+        uint256 _id = 0;
+        uint256 _min = station.getGameMeta(_id).minAmount;
+        uint256 amount = _number < _min ? vm.randomUint(_min, 255) : _number;
+        amount *= 77;
+
+        buyCookie(amount);
+        approveCookie(amount);
+        // TODO mock으로 결과 강제하기
+        // 현재는 높은 확률로 성공하도록 함
+        for (uint256 i = 0; i < 77; ++i) {
+            playRPS(amount / 77, _id, 1);
+        }
+        uint future = getBlockNumber() + 4;
+        vm.setBlockhash(future, blockHash);
+        vm.warp(future);
+
+        claim_asIs();
+        claim();
+        claim_toBe();
     }
 }
