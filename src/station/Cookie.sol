9782da0563b86b4d09497796b47c2481fbf06767
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2024-09-02 23:31:13 +0900

    📝 Docs : README.md

--- src/station/Cookie.sol
+++ src/station/Cookie.sol
@@ -5,8 +5,8 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Cookie is ERC20, Ownable {
-    constructor() ERC20("Cookie", "CKE") Ownable(msg.sender) {
+contract Cookie is ERC20 {
+    constructor() ERC20("Cookie", "CKE") {
         _mint(msg.sender, type(uint256).max);
     }
 }
